59cb2424b21a92340b09cdefbb8a4402e05b81e3
James Weatherley <james@weatherley.net>
2025-11-06 02:48:58 +0000

    Initial Commit

---
+++ KochSnowflake/KochSnowflakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KochSnowflakeApp.swift
+//  KochSnowflake
+//
+//  Created by James Weatherley on 06/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct KochSnowflakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KochSnowflakeTests/KochSnowflakeTests.swift
@@ -0,0 +1,17 @@
+//
+//  KochSnowflakeTests.swift
+//  KochSnowflakeTests
+//
+//  Created by James Weatherley on 06/11/2025.
+//
+
+import Testing
+@testable import KochSnowflake
+
+struct KochSnowflakeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
