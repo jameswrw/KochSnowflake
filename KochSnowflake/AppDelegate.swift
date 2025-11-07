4527e17810aef37a18f26f70a0ec0d487b3bc4c0
James Weatherley <james@weatherley.net>
2025-11-07 06:52:19 +0000

    Quit on closing the last window

---
+++ KochSnowflake/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  KochSnowflake
+//
+//  Created by James Weatherley on 07/11/2025.
+//
+
+import AppKit
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        true
+    }
+}
+
--- KochSnowflake/KochSnowflakeApp.swift
+++ KochSnowflake/KochSnowflakeApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct KochSnowflakeApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
